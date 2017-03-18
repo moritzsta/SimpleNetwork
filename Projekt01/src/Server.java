a97a7cab6358886026f873dfccd9e5c9382d9efe
ms <stabler.moritz@gmail.com>
2017-03-18 16:36:11 +0100

    added server

---
+++ Projekt01/src/Server.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+    final int PORT_NUMBER = 44827;
+
+    while(true) {
+        try {
+        //Listen on port
+        ServerSocket serverSock = new ServerSocket(PORT_NUMBER);
+        System.out.println("Listening...");
+
+        //Get connection
+        Socket clientSock = serverSock.accept();
+        System.out.println("Connected client");
+
+        //Get input 
+        BufferedReader br = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
+        System.out.println(br.readLine());
+
+        br.close();
+        serverSock.close();
+        clientSock.close();
+        } catch(Exception e) {
+        e.printStackTrace();
+        }
+    }
+    }
+}
