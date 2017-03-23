536d1e12bdad985d4f80fb0dfa7a4e4a0976cdd9
ms <ms@GamingPCms.fritz.box>
2017-03-23 16:35:29 +0100

    object namen angepasst

--- Projekt01/src/Server.java
+++ Projekt01/src/Server.java
@@ -2,30 +2,35 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    public static void main(String[] args) {
-    final int PORT_NUMBER = 44827;
+	public static void main(String[] args) {
+		final int PORT_NUMBER = 44827;
+		ServerSocket serverSock = null;
+		Socket clientSock = null;
+		DataInputStream input;
 
-    while(true) {
-        try {
-        //Listen on port
-        ServerSocket serverSock = new ServerSocket(PORT_NUMBER);
-        System.out.println("Listening...");
+		while (true) {
+			try {
+				// Listen on port
+				serverSock = new ServerSocket(PORT_NUMBER);
+				System.out.println("Listening...");
 
-        //Get connection
-        Socket clientSock = serverSock.accept();
-        System.out.println("Connected client");
+				// Get connection
+				clientSock = serverSock.accept();
+				System.out.println("Connected client");
 
-        //Get input 
-        BufferedReader br = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
-        System.out.println(br.readLine());
-        System.out.println(clientSock.getInetAddress());
+				// Get input
+				// BufferedReader br = new BufferedReader(new
+				// InputStreamReader(clientSock.getInputStream()));
 
-        br.close();
-        serverSock.close();
-        clientSock.close();
-        } catch(Exception e) {
-        e.printStackTrace();
-        }
-    }
-    }
+				input = new DataInputStream(clientSock.getInputStream());
+				System.out.println(input);
+				System.out.println(clientSock.getInetAddress() + " connected");
+
+				serverSock.close();
+				clientSock.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
